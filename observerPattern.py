e329c6e2debe9cf71696fb1f86731426b8ca4337
scott1028 <mic1028002@gmail.com>
2013-12-06 23:27:25 +0800

    first commit

---
+++ observerPattern.py
@@ -0,0 +1,49 @@
+#coding:utf-8
+# 說明：
+#	觀察者模式（有時又被稱為發布/訂閱模式）是軟體設計模式的一種。
+#	在此種模式中，一個目標物件管理所有相依於它的觀察者物件，並且在它本身的狀態改變時主動發出通知。
+#	這通常透過呼叫各觀察者所提供的方法來實現。此種模式通常被用來實作事件處理系統。
+
+
+# 觀察者：當被觀察的物件發送事件的時候觀察者會收到訊息
+class Observer(object):
+	def __init__(self,name):
+		self.name=name
+
+	# 當官者被提醒的時候
+	def notify(self,event):
+		print self.name+' observer is notified by Event: '+event
+
+# 目標物件：內涵所有觀察者的指標並統一管理。
+class Component(object):
+	# 參考 js 部分把觀察者管理模式寫入目標物件
+	def addObserver(self,observer):
+		self.observers.append(observer)
+
+	def removeObserver(self,observer):
+		self.observers.remove(observer)
+
+	def trigger(self,event):
+		for i in self.observers:
+			i.notify(event)
+
+	def __init__(self):
+		self.observers=[]
+
+# 建立觀察者
+observerWindow=Observer('window')
+observerPanel=Observer('panel')
+
+# 建立一個元件然後增加他的觀察者
+componentA=Component()
+
+# 新增觀察者
+componentA.addObserver(observerWindow)
+componentA.addObserver(observerPanel)
+
+# 移除觀察者
+componentA.removeObserver(observerWindow)
+
+componentA.trigger('click')
+
+
