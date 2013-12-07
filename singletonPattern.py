0eaa41a243996166aa928b31444e73c3f047e04d
scott1028 <mic1028002@gmail.com>
2013-12-07 09:24:51 +0800

    modified:   singletonPattern.py

--- singletonPattern.py
+++ singletonPattern.py
@@ -5,6 +5,7 @@
 #	簡單來說就是不允許你建立第二個 instance。使用 static class 來達成此目的
 #	基本上不要使用 new mainSystem() 的方式來建立。
 #	就如一個系統資料庫存取對象只需要一個，如果每個功能都建立一個，就是浪費資源。雖然也可以用 Global 變數來達成，但是就失去OOP的精神了！而且也比較沒有架構。
+#	就我個人感覺上 ORM 可以這樣設計。
 
 class mainSystem(object):
 	# 可以直接透過 mainSystem 當作 static variable
