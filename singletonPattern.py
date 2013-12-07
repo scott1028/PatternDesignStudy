501f61293f6a7dcd4bfd6413d68c8dbf6f7c3b19
scott1028 <mic1028002@gmail.com>
2013-12-07 09:19:08 +0800

    modified:   singletonPattern.py

--- singletonPattern.py
+++ singletonPattern.py
@@ -4,6 +4,7 @@
 #	實現單例模式的思路是：一個類能返回對象一個引用(永遠是同一個)和一個獲得該實例的方法（必須是靜態方法，通常使用getInstance這個名稱）
 #	簡單來說就是不允許你建立第二個 instance。使用 static class 來達成此目的
 #	基本上不要使用 new mainSystem() 的方式來建立。
+#	就如一個系統資料庫存取對象只需要一個，如果每個功能都建立一個，就是浪費資源。雖然也可以用 Global 變數來達成，但是就失去OOP的精神了！而且也比較沒有架構。
 
 class mainSystem(object):
 	# 可以直接透過 mainSystem 當作 static variable
